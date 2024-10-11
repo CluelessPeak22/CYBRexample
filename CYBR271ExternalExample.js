557bafc0e4dcedece66b117c3abe9a1d0146a069
CluelessPeak22 <113392363+CluelessPeak22@users.noreply.github.com>
2024-10-11 14:52:18 +1300

    Update and rename CYBR271ExternalExample to CYBR271ExternalExample.js

---
+++ CYBR271ExternalExample.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to display an alert message
+function showAlert() {
+    alert('XSS: "Hi, this is external and being hosted from GITHUB!');
+}
+
+// Call the function to display the alert
+showAlert();

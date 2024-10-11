4b3a3579f71ee472be50592db28d630bf487557d
CluelessPeak22 <113392363+CluelessPeak22@users.noreply.github.com>
2024-10-11 17:08:27 +1300

    Update CYBR271ExternalExample.js

--- CYBR271ExternalExample.js
+++ CYBR271ExternalExample.js
@@ -1,6 +1,8 @@
-// Function to display an alert message
+// Function to display an alert message with the user's profile link
 function showAlert() {
-    alert('XSS: "Hi, this is external and being hosted from GITHUB!');
+    var userGuid = elgg.session.user.guid; // Get the current user's GUID
+    var profileUrl = "http://your-elgg-site/profile/view/" + userGuid; // Construct the profile URL
+    alert("Your profile URL is: " + profileUrl); // Display the alert message
 }
 
 // Call the function to display the alert
